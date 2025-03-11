d9743649ab766cc95a133c3454ee51ed47ec33bc
Argeu Venturini <argeu.v@hotmail.com>
2025-03-11 00:34:13 -0300

    Adding a Drawer and initial Settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
+import 'screens/settings_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 import 'screens/tabs_screen.dart';
@@ -65,6 +65,8 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
+
       },
     );
   }
